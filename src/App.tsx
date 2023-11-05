3bee3370ac00d47979b7770d974d0c067e01c78e
acharayaP03 <acharyaP03@gmail.com>
2023-11-05 19:18:46 +1100

    initial commit with project config

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import * as esbuild from "esbuild-wasm";
+import React, { useState, useEffect, useRef } from "react";
+
+
+const App = () => {
+    const [input, setInput] = useState(""); 
+    const [code, setCode] = useState("");
+    const ref = useRef<any>();
+
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+      
+    };
+
+    useEffect(() => { startService() }, []);
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        });
+
+        console.log(result);
+        
+        setCode(result.code);
+    }
+    return (
+    
+        <div>
+            <textarea value={input} onChange={e => setInput(e.target.value)}/>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>
+            {code}
+            </pre>
+        </div>
+    )
+}
+
+export default App; 
