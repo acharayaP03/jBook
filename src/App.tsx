15d2138f8e05523c98a3d438413a90d63d1c1daf
acharayaP03 <acharyaP03@gmail.com>
2024-11-19 22:20:14 +1100

    update packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,44 @@
 import * as esbuild from "esbuild-wasm";
 import React, { useState, useEffect, useRef } from "react";
 
-
 const App = () => {
-    const [input, setInput] = useState(""); 
-    const [code, setCode] = useState("");
-    const ref = useRef<any>();
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const ref = useRef<any>();
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: '/esbuild.wasm'
-        });
-      
-    };
+  const startService = async () => {
+    ref.current = await esbuild.initialize({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
 
-    useEffect(() => { startService() }, []);
-    const onClick = async () => {
-        if (!ref.current) {
-            return;
-        }
-        const result = await ref.current.transform(input, {
-            loader: 'jsx',
-            target: 'es2015'
-        });
+  useEffect(() => {
+    startService();
+  }, []);
 
-        console.log(result);
-        
-        setCode(result.code);
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
     }
-    return (
-    
-        <div>
-            <textarea value={input} onChange={e => setInput(e.target.value)}/>
-            <div>
-                <button onClick={onClick}>Submit</button>
-            </div>
-            <pre>
-            {code}
-            </pre>
-        </div>
-    )
-}
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
 
-export default App; 
+    console.log(result);
+
+    setCode(result.code);
+  };
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+export default App;
